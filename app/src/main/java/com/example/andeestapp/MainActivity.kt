d3ba9a8d5942ad4ddf8273bd1e3a821e74d14679
AndresRubioS <andres-rubio-salas@hotmail.com>
2022-11-14 09:59:23 +0100

    imagen en addcampo

--- app/src/main/java/com/example/andeestapp/MainActivity.kt
+++ app/src/main/java/com/example/andeestapp/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.andeestapp.databinding.ActivityMainBinding
+
 import com.example.andeestapp.galery.GaleriaActivity
 import com.example.andeestapp.galery.GaleriaViewHolder
 
