5c45f381b0103d90296b5408d0c46d3f4ad3e7c2
AndresRubioS <andres-rubio-salas@hotmail.com>
2023-02-14 14:00:35 +0100

    widget

--- app/src/main/java/com/example/andeestapp/MensajesMuchachitaWidget.kt
+++ app/src/main/java/com/example/andeestapp/MensajesMuchachitaWidget.kt
@@ -9,11 +9,7 @@ import android.widget.RemoteViews
  * Implementation of App Widget functionality.
  */
 class MensajesMuchachitaWidget : AppWidgetProvider() {
-    override fun onUpdate(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetIds: IntArray
-    ) {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
@@ -29,11 +25,7 @@ class MensajesMuchachitaWidget : AppWidgetProvider() {
     }
 }
 
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
+internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
     val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.mensajes_muchachita_widget)
